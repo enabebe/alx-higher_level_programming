b7a39f3c1d4d85f6715b80ee3373d907f96400e8
enabebe <nathabe984@gmail.com>
2021-12-21 22:20:18 +0300

    file added

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
-from sys import argv
-"""access commandline arguments"""
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""create object from JSON file"""
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-"""writes an object to text file, using JSON representation"""
+"""Module 9-add_item.
+Adds all arguments to a Python list,
+and then save them to a file.
+"""
 
-filename = "add_item.json"
-try:
-    content = load_from_json_file(filename)
-except:
-    content = []
+import sys
+import json
+import os.path
 
-for i in range(1, len(argv)):
-    content.append(argv[i])
-save_to_json_file(content, filename)
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+
+my_file = 'add_item.json'
+
+my_list = []
+
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)
